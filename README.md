fdef343529d0b387d8aad0a30013348af25e60ee
Artur Tarasenko <arttarawork@gmail.com>
2017-05-15 02:59:07 +0800

    More stuff

--- README.md
+++ README.md
@@ -8,6 +8,18 @@ Built in python, powered by Markovify and Tweepy.
 
 Written by Artur Tarasenko for COMP 3211 for Spring 2017 at HKUST.
 
+## System Details
+
+This was run on Ubuntu 16.04 LTS and Python 2.7
+
+## Installation
+
+*Make sure you have Python 2.7 installed
+
+*Follow the [tweepy]{https://github.com/tweepy/tweepy} installation requirements
+
+*Follow the [markovify]{https://github.com/jsvine/markovify} installation requirements
+
 Resources used:
 
 https://github.com/jsvine/markovify
--- runMe.py
+++ runMe.py
@@ -9,11 +9,20 @@ consumer_secret = ""
 access_key = ""
 access_secret = ""
 
-if (!(consumer_key | consumer_secret | access_key | access_secret)):
+if ((not consumer_key) | (not consumer_secret) | (not access_key) | (not access_secret)):
 	print "You will be prompted for your Twitter API details, but you can change the string value for them inside the runMe.py file.\nYou can find these settings at https://dev.twitter.com/apps\n"
 
-if(!consumer_key):
-	print "Consumer Key: "
+if (not consumer_key):
+	consumer_key = raw_input("Consumer Key: ")
+
+if (not consumer_secret):
+	consumer_secret = raw_input("Consumer Secret: ")
+
+if (not access_key):
+	access_key = raw_input("Access Key: ")
+
+if (not access_secret):
+	access_secret = raw_input("Access Secret: ")
 
 
 
