2e5faa50c0de0b8d934490a101be2e4522e334d3
Artur Tarasenko <arttarawork@gmail.com>
2017-05-15 04:08:30 +0800

    Finished

--- runMe.py
+++ runMe.py
@@ -4,10 +4,14 @@ import operator
 import tweepy		#https://github.com/tweepy/tweepy
 import csv
 
+# Twitter API authentication
+
 consumer_key = ""
 consumer_secret = ""
 access_key = ""
 access_secret = ""
+
+# if authenication isn't provided, it's prompted here
 
 if ((not consumer_key) | (not consumer_secret) | (not access_key) | (not access_secret)):
 	print "You will be prompted for your Twitter API details, but you can change the string value for them inside the runMe.py file.\nYou can find these settings at https://dev.twitter.com/apps\n"
@@ -24,9 +28,39 @@ if (not access_key):
 if (not access_secret):
 	access_secret = raw_input("Access Secret: ")
 
+#Authenticates
 
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+api = tweepy.API(auth)
 
-with open(os.path.join(os.path.dirname(__file__), "markovify/test/texts/sherlock.txt")) as f:
-    sherlock = f.read()
-    sherlock_model = markovify.Text(sherlock)
-    print(sherlock_model.make_sentence())
+handle = raw_input("Twitter Username: ")
+
+#Compiles tweets
+
+total_tweets = []
+
+next_tweets = api.user_timeline(screen_name = handle, count = 200)
+
+total_tweets.extend(next_tweets)
+
+while len(next_tweets) > 0:
+
+	next_tweets = api.user_timeline(screen_name = handle, count = 200, max_id = total_tweets[-1].id - 1)
+
+	total_tweets.extend(next_tweets)
+
+#Generates string of corpus
+
+corpus = ""
+
+for tweet in total_tweets:
+	corpus += (tweet.text + " ")
+
+#Generates markov model of corpus
+
+markov_model = markovify.Text(corpus)
+
+#Prints sentence
+
+print(markov_model.make_sentence())
